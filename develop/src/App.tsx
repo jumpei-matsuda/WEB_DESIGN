e2dace7d2d702947615b9bbe22b70c0890bf534e
jumpei-matsuda <jumpei.0022.matsuda@gmai.com>
2021-07-31 14:04:21 +0900

    develop ブランチ戦略

---
+++ develop/src/App.tsx
@@ -0,0 +1,22 @@
+// import React, { useEffect } from 'react';
+// import { Switch, Route, useHistory, useLocation } from 'react-router';
+// import { ThemeProvider } from '@material-ui/core/styles';
+// import useMediaQuery from '@material-ui/core/useMediaQuery';
+
+const App: React.FC = () => (
+  // const { hash, pathname } = useLocation();
+  // const { action } = useHistory();
+
+  // スクロール位置リセット
+  // useEffect(() => {
+  //   if (!hash || action !== 'POP') {
+  //     window.scrollTo(0, 0);
+  //   }
+  // }, [action, hash, pathname]);
+
+  // レスポンシブ対応（ブレークポイント：600px）
+  // const isResponsible = useMediaQuery(theme.breakpoints.down('sm'));
+
+  <h1>Hello Reacts</h1>
+);
+export default App;
